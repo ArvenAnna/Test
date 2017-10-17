522133a97ecdedaf278d7d65b272d652ee3b36e7
Yaroslav Ishchuk <siavun811@gmail.com>
2017-10-17 21:51:10 +0300

    NPG

---
+++ InitilizingBlocks/src/com/mainacad/Main.java
@@ -0,0 +1,10 @@
+package com.mainacad;
+
+/**
+ * Created by User on 17/10/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyInitTest mit = new MyInitTest();
+    }
+}
--- InitilizingBlocks/src/com/mainacad/MyInitTest.java
+++ InitilizingBlocks/src/com/mainacad/MyInitTest.java
@@ -1,5 +1,43 @@
 package com.mainacad;
 
-public class MyInitTest{
+public class MyInitTest {
 
+    public static int height1;
+    public static int weight1;
+    private int height;
+    private int weight;
+
+    static {
+        height1 = 14;
+        System.out.println("static initialization block 1");
+    }
+
+    static {
+        weight1 = 28;
+        System.out.println("static initialization block 2");
+    }
+
+    {
+        height = 15;
+        System.out.println("non-static initialization block 1");
+    }
+
+    {
+        weight = 29;
+        System.out.println("non-static initialization block 2");
+    }
+
+
+    public MyInitTest() {
+        this(30);
+        height = 30;
+        System.out.println("constructor 2");
+
+
+    }
+
+    public MyInitTest(int height) {
+        this.height = height;
+        System.out.println("constructor 1");
+    }
 }
