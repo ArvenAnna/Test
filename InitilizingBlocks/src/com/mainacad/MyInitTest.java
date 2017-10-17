26e650234c76fc0ae61b742d6ca8985ae3615d5e
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-10-17 21:26:27 +0300

    commit

---
+++ InitilizingBlocks/src/com/mainacad/MyInitTest.java
@@ -0,0 +1,13 @@
+package com.mainacad;
+
+public class MyInitTest extends MyInitTestParent {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    MyInitTest(int id) {
+        this.id = id;
+    }
+}
