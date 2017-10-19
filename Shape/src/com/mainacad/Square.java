298bedcd0a078aa33a9013be6002a0b8d35d1fdc
sasha.zhurylo <sasha.zhurylo@gmail.com>
2017-10-19 15:46:44 +0300

    create new abstract class Realisation

--- Shape/src/com/mainacad/Square.java
+++ Shape/src/com/mainacad/Square.java
@@ -2,44 +2,10 @@ package com.mainacad;
 
 import javafx.scene.paint.Color;
 
-public class Square implements Constructable, Paintable {
-    private static final int POINTS_COUNT = 4;
-    private final Point[] points ;
+public class Square extends Realisation  {
 
     public Square(Point[] points) {
-        if (points.length != POINTS_COUNT) {
-            throw new IllegalArgumentException("Square must contain 4 point");
-        }
-        Point[] copyPoints = new Point[POINTS_COUNT];
-        int i = 0;
-        for (Point point : points) {
-            copyPoints[i] = new Point(point.getX(), point.getY());
-            i++;
-        }
-        this.points = copyPoints;
-    }
-
-    @Override
-    public double areaShape() {
-        Otrezok otrezok = new Otrezok();
-        double area = 0;
-        for (int i = 1; i < POINTS_COUNT - 1; i++) {
-            area += otrezok.lang(points[i - 1].getX(), points[0].getX(), points[i - 1].getY(), points[0].getY());
-        }
-        area *= otrezok.lang(points[POINTS_COUNT - 1].getX(), points[0].getX(), points[POINTS_COUNT - 1].getY(), points[0].getY());
-        return area;
-    }
-
-
-    @Override
-    public double perimetrShape() {
-        Otrezok otr = new Otrezok();
-        double perimeter = 0.0;
-        for (int i = 1; i < POINTS_COUNT; i++) {
-            perimeter += otr.lang(points[i - 1].getX(), points[i].getX(), points[i - 1].getY(), points[i].getY());
-        }
-        perimeter += otr.lang(points[POINTS_COUNT - 1].getX(), points[0].getX(), points[POINTS_COUNT - 1].getY(), points[0].getY());
-        return perimeter;
+        super(points,4);
     }
 
     @Override
@@ -52,14 +18,5 @@ public class Square implements Constructable, Paintable {
         return Color.CORAL;
     }
 
-    @Override
-    public Point[] getPoints() {
-        Point[] copyPoints = new Point[POINTS_COUNT];
-        int i = 0;
-        for (Point point : points) {
-            copyPoints[i] = new Point(point.getX(), point.getY());
-            i++;
-        }
-        return copyPoints;
-    }
+
 }
