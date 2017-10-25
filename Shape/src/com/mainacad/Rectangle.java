dc41653a769021c9db7eb1b9f51bb6a535dce2c4
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-10-25 23:12:14 +0300

    Zhuravel ANdrey: add comments to Rectangle shape

--- Shape/src/com/mainacad/Rectangle.java
+++ Shape/src/com/mainacad/Rectangle.java
@@ -2,15 +2,21 @@ package com.mainacad;
 
 import javafx.scene.paint.Color;
 
+/**
+ * This class create Rectangle shape using Points in Shape class and use paintingShape method and Color method
+ */
+
 public class Rectangle extends Shape { //changed by Zhuravel Andrey, inheritance from Shape class)
 
-    public Rectangle (Point[] points) {
-        super(points,4);
+    public Rectangle(Point[] points) {
+        super(points, 4);
     }
+
     @Override
     public void paintingShape() {
         System.out.println("This is my rectangle");
     }
+
     @Override
     public Color getColor() {
         return Color.GREEN;
