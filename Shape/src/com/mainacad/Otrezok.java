fe03a5e2161aec61254ef562d60806d37aaa2ba8
ImDyA <vasurman13@gmail.com>
2017-10-27 18:19:00 +0300

    Alexey

--- Shape/src/com/mainacad/Otrezok.java
+++ Shape/src/com/mainacad/Otrezok.java
@@ -14,10 +14,6 @@ public class Otrezok {
     }
 
     public double lang(double x, double x1, double y, double y1) {
-//        this.x = x;
-//        this.x1 = x1;
-//        this.y = y;
-//        this.y1 = y1;
         double xy = Math.sqrt(((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y)));
         return xy;
     }
