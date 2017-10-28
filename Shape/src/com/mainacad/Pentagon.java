d2375669c831a7233ebc54d29569747aef022a09
southlandkarn <southlandkarn@gmail.com>
2017-10-28 14:56:58 +0300

    change Pentagon

--- Shape/src/com/mainacad/Pentagon.java
+++ Shape/src/com/mainacad/Pentagon.java
@@ -1,26 +1,33 @@
 package com.mainacad;
-//
-public class Pentagon implements Constructable {
-    private int heightPentagon;
 
-    public int getHeightPentagon(){
-        return heightPentagon;
-    }
+import javafx.scene.paint.Color;
 
-    public void setHeightPentagon(int heightPentagon){
-        this.heightPentagon=heightPentagon;
-    }
+// This class create Pentagon using Shape class and Overriding areaShape() method
 
-    @Override
-    public double areaShape(){
-        return (heightPentagon * heightPentagon)/4 * (Math.sqrt(25+10 * Math.sqrt(5)));
+public class Pentagon extends Shape{
+
+    public Pentagon(Point[] points) {
+
+        super(points, 5);
     }
     @Override
-    public double perimetrShape(){
-        return heightPentagon * 5;
+    public double areaShape() {
+        Otrezok otrezok = new Otrezok(12.5, 56.1, 4.8, 56.6);
+        double area = 0;
+        for (int i = 1; i < POINTS_COUNT - 1; i++) {
+            area += otrezok.lang(points[i - 1].getX(), points[0].getX(), points[i - 1].getY(), points[0].getY());
+        }
+        area *= otrezok.lang(points[POINTS_COUNT - 1].getX(), points[0].getX(), points[POINTS_COUNT - 1].getY(), points[0].getY())/4 * (Math.sqrt(25+10 * Math.sqrt(5)));
+
+        return area;
     }
+
     @Override
     public void paintingShape(){
         System.out.println("You painting my pentagon");
+    }
+    @Override
+    public Color getColor() {
+        return Color.GREEN;
     }
 }
