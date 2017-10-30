35d9def719ed4963d84a5095ba7a47e33cc3e04a
southlandkarn <southlandkarn@gmail.com>
2017-10-30 13:25:55 +0200

    add Override equals, hashCode

--- Inheritance/src/com/example/common/Device.java
+++ Inheritance/src/com/example/common/Device.java
@@ -37,5 +37,26 @@ public class Device {
         System.out.println("serialnumber");
         System.out.println("price");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Device device = (Device) o;
+
+        if (Float.compare(device.price, price) != 0) return false;
+        if (manufacturer != null ? !manufacturer.equals(device.manufacturer) : device.manufacturer != null)
+            return false;
+        return serialNumber != null ? serialNumber.equals(device.serialNumber) : device.serialNumber == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = manufacturer != null ? manufacturer.hashCode() : 0;
+        result = 31 * result + (price != +0.0f ? Float.floatToIntBits(price) : 0);
+        result = 31 * result + (serialNumber != null ? serialNumber.hashCode() : 0);
+        return result;
+    }
 }
 
