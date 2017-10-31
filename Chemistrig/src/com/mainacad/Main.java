5b81594acaaa5d41fc8ebe448a097ce0cd3442b5
sasha96 <sasha.zhurylo@gmail.com>
2017-10-31 15:30:32 +0200

    little change

--- Chemistrig/src/com/mainacad/Main.java
+++ Chemistrig/src/com/mainacad/Main.java
@@ -12,12 +12,16 @@ import static java.lang.Enum.valueOf;
 
 public class Main {
     public static void main(String[] args) {
-        ChemicalElement object = new ChemicalElement("Na", 6, 3);
+        ChemicalElement object = new ChemicalElement("Li", 3, 3);
         ChemicalElement object2 = new ChemicalElement("Sn", 4, 10);
-        ChemicalElement object3 = new ChemicalElement("K", 5, 2);
+        ChemicalElement object3 = new ChemicalElement("C", 5, 2);
         ChemicalElement[] arr = {object, object2, object3};
 
-        //  Arrays.sort(arr, new ComparatorRlectroNegatinity());
+        //Arrays.sort(arr, new ComparatorRlectroNegatinity());
+
+        ChemicalElement.isAlcaiMetals(object2);
+        ChemicalElement.isMetalSare(object2);
+        ChemicalElement.isTransitionMetals(object2);
 
         Arrays.sort(arr, new Comparator<ChemicalElement>() {
                     @Override
@@ -25,6 +29,10 @@ public class Main {
                         if (o1.name.equals("C") || o2.name.equals("C")) {
                             NotMetall.C.setElecrtonegatyvity(1);
                             NotMetall.H.setElecrtonegatyvity(2);
+                            if (o1.atomicNumber > o2.atomicNumber)
+                                return -1;
+                            if (o2.atomicNumber > o1.atomicNumber)
+                                return 1;
                         }
                         if (o1.atomicNumber > o2.atomicNumber)
                             return -1;
