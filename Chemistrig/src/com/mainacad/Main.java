694fb72301def5e54df98d91c3839371554f1cb7
Zhuravel Andrey <zhuravel.andrey@gmail.com>
2017-11-02 20:38:46 +0200

    comparator add

--- Chemistrig/src/com/mainacad/Main.java
+++ Chemistrig/src/com/mainacad/Main.java
@@ -15,25 +15,25 @@ public class Main {
         ChemicalElement[] arr = {object,object2,object3,object4, object5, object6};
 
 
-        // Arrays.sort(arr, new ComparatorRlectroNegatinity());
+         Arrays.sort(arr, new ComparatorRlectroNegatinity());
 
-        Arrays.sort(arr, new Comparator<ChemicalElement>() {
-                    @Override
-                    public int compare(ChemicalElement o1, ChemicalElement o2) {
-                        if (o1.getName().equals("C")) {
-                            return -1;
-                        } else if (o2.getName().equals("C")) {
-                            return 1;
-                        } else if (o1.getElecrtonegatyvity() > o2.getElecrtonegatyvity()) {
-                            return 1;
-                        } else if (o2.getElecrtonegatyvity() > o1.getElecrtonegatyvity()) {
-                            return -1;
-                        }
-                        return 0;
-
-                    }
-                }
-        );
+//        Arrays.sort(arr, new Comparator<ChemicalElement>() {
+//                    @Override
+//                    public int compare(ChemicalElement o1, ChemicalElement o2) {
+//                        if (o1.getName().equals("C")) {
+//                            return -1;
+//                        } else if (o2.getName().equals("C")) {
+//                            return 1;
+//                        } else if (o1.getElecrtonegatyvity() > o2.getElecrtonegatyvity()) {
+//                            return 1;
+//                        } else if (o2.getElecrtonegatyvity() > o1.getElecrtonegatyvity()) {
+//                            return -1;
+//                        }
+//                        return 0;
+//
+//                    }
+//                }
+//        );
 
         for (ChemicalElement r : arr) {
             System.out.println(r);
